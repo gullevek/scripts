16f197412120901ed7af2907907098638ecbccf6
Clemens Schwaighofer <gullevek@gullevek.org>
2015-01-15 11:31:57 +0900

    Update pg restore from file script
    - Add option to set base dump folder path
    - Add option to set redhat or debian folder layout for binaries
    - Add import of globals before importing any data (can be triggered to off if needed)

--- dump_database/pg_restore_db_file.sh
+++ dump_database/pg_restore_db_file.sh
@@ -1,40 +1,94 @@
 #!/bin/bash
 
-DUMP_FOLDER='/<source folder>/';
-DBPATH_BASE='/usr/lib/postgresql/';
-#DBPATH_BASE='/usr/pgsql-'
-DBPATH_VERSION='9.3/';
-DBPATH_BIN='bin/';
-DROPDB="dropdb";
-CREATEDB="createdb";
-CREATELANG="createlang";
-PGRESTORE="pg_restore";
-CREATEUSER="createuser";
-PSQL="psql";
-PORT=5432;
-HOST='localhost';
+function usage ()
+{
+	cat <<- EOT
+	Usage: ${0##/*/} -f <dump folder> [-r] [-g]
+
+	-f: dump folder source. Where the database dump files are located. This is a must set option
+	-r: use redhat base paths instead of debian
+	-g: do not import globals file
+	EOT
+}
+
+REDHAT=0;
+IMPORT_GLOBALS=1;
+DUMP_FOLDER='';
+while getopts ":f:gr" opt
+do
+	case $opt in
+		f|file)
+			DUMP_FOLDER=$OPTARG;
+			;;
+		g|globals)
+			IMPORT_GLOBALS=0;
+			;;
+		r|redhat)
+			REDHAT=1;
+			;;
+		h|help)
+            usage;
+            exit 0;
+            ;;
+        \?)
+            echo -e "\n Option does not exist: $OPTARG\n";
+            usage;
+            exit 1;
+			;;
+	esac;
+done;
+
+if [ "$REDHAT" -eq 1 ];
+then
+	# Redhat base path (for non official ones would be '/usr/pgsql-'
+	DBPATH_BASE='/usr/pgsql-'
+else
+	# Debian base path
+	DBPATH_BASE='/usr/lib/postgresql/';
+fi;
+
 LOGS=$DUMP_FOLDER'logs/';
-EXCLUDE_LIST="pg_globals"; # space separated
-LOGFILE="tee -a $LOGS/PG_RESTORE_DB_FILE.`date +"%F_%T"`.log";
+
+if [ "$DUMP_FOLDER" = '' ];
+then
+	echo "Please provide a source folder for the dump files with the -f option";
+	exit;
+fi;
 
 # check that source folder is there
 if [ ! -d "$DUMP_FOLDER" ];
 then
-	echo "Folder $DUMP_FOLDER does not exist";
+	echo "Folder '$DUMP_FOLDER' does not exist";
 	exit;
 fi;
 
 # create logs folder if missing
 if [ ! -d "$LOGS" ];
 then
-	echo "Creating $LOGS folder";
+	echo "Creating '$LOGS' folder";
 	mkdir -p "$LOGS";
 	if [ ! -d "$LOGS" ];
 	then
-		echo "Creation of $LOGS folder failed";
+		echo "Creation of '$LOGS' folder failed";
 		exit;
 	fi;
 fi;
+
+# default version (for folder)
+DBPATH_VERSION='9.4/';
+DBPATH_BIN='bin/';
+# postgresql binaries
+DROPDB="dropdb";
+CREATEDB="createdb";
+CREATELANG="createlang";
+PGRESTORE="pg_restore";
+CREATEUSER="createuser";
+PSQL="psql";
+# default port and host
+PORT=5432;
+HOST='localhost';
+EXCLUDE_LIST="pg_globals"; # space separated
+LOGFILE="tee -a $LOGS/PG_RESTORE_DB_FILE.`date +"%F_%T"`.log";
 
 # just set port & host for internal use
 port='-p '$PORT;
@@ -53,6 +107,31 @@ pos=1;
 # go through all the files an import them into the database
 MASTERSTART=`date +'%s'`;
 master_start_time=`date +"%F %T"`;
+# first import the pg_globals file if this is requested, default is yes
+if [ "$IMPORT_GLOBALS" -eq 1 ];
+then
+	start_time=`date +"%F %T"`;
+	START=`date +'%s'`;
+	# get the pg_globals file
+	echo "=[Globals Restore]=START=[$start_time]==================================================>" | $LOGFILE;
+	# get newest and only the first one
+	file=`ls -1t $DUMP_FOLDER/pg_global* | head -1`;
+	filename=`basename $file`;
+	version=`echo $filename | cut -d "." -f 3 | cut -d "-" -f 2`; # db version, without prefix of DB type
+	version=$version'.'`echo $filename | cut -d "." -f 4 | cut -d "_" -f 1`; # db version, second part (after .)
+	# create the path to the DB from the DB version in the backup file
+	if [ ! -z "$version" ];
+	then
+		DBPATH_VERSION_LOCAL=$version'/';
+	else
+		DBPATH_VERSION_LOCAL=$DBPATH_VERSION;
+	fi;
+	DBPATH=$DBPATH_BASE$DBPATH_VERSION_LOCAL$DBPATH_BIN;
+	echo "+ Restore globals file: $filename to [$_host:$_port] @ `date +"%F %T"`" | $LOGFILE;
+	$DBPATH$PSQL -U postgres $host $port -f $file -e -q -X template1 | $LOGFILE;
+	DURATION=$[ `date +'%s'`-$START ];
+	printf "=[Globals Restore]=END===[%5s seconds]========================================================>\n" $DURATION | $LOGFILE;
+fi;
 for file in $DUMP_FOLDER/*.sql;
 do
 	start_time=`date +"%F %T"`;
