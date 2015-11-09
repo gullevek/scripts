db898c1adb9e2f6f3f940efd0ed4c46afa2e5c8e
Clemens Schwaighofer <gullevek@gullevek.org>
2015-11-09 15:04:59 +0900

    Remote sync script update with dry run and bash formatting
    add -n dry run to test setup before starting a real run
    changed all variable and escape command settings to proper formatted
    one.

--- remote_backup/sync_backup.sh
+++ remote_backup/sync_backup.sh
@@ -9,9 +9,10 @@ set -e
 function usage ()
 {
 	cat <<- EOT
-	Usage: ${0##/*/} [-v] [-x] [-c] [-s <source folder>] [-t <target folder>]
+	Usage: ${0##/*/} [-v] [-x] [-c] [-n] [-s <source folder>] [-t <target folder>]
 
 	-v: verbose output, for use outside scripted runs
+	-n: dry run
 	-x: add -X and -A rsync flag for extended attributes. Can oops certain kernels.
 	-l: log file name, if not set default name is used
 	-s: source folder, must exist
@@ -36,12 +37,12 @@ function convert_time
 		BC_OK=0;
 	fi;
 	# input time stamp
-	timestamp=$1;
+	timestamp=${1};
 	# round to four digits for ms
-	timestamp=`printf "%1.4f" $timestamp`;
+	timestamp=$(printf "%1.4f" ${timestamp});
 	# get the ms part and remove any leading 0
-	ms=`echo $timestamp | cut -d "." -f 2 | sed -e 's/^0*//'`;
-	timestamp=`echo $timestamp | cut -d "." -f 1`;
+	ms=$(echo ${timestamp} | cut -d "." -f 2 | sed -e 's/^0*//');
+	timestamp=$(echo ${timestamp} | cut -d "." -f 1);
 	timegroups=(86400 3600 60 1); # day, hour, min, sec
 	timenames=("d" "h" "m" "s"); # day, hour, min, sec
 	output=( );
@@ -49,44 +50,45 @@ function convert_time
 	for timeslice in ${timegroups[@]};
 	do
 		# floor for the division, push to output
-		if [ $BC_OK -eq 1 ];
+		if [ ${BC_OK} -eq 1 ];
 		then
-			output[${#output[*]}]=`echo "$timestamp/$timeslice" | bc`;
-			timestamp=`echo "$timestamp%$timeslice" | bc`;
+			output[${#output[*]}]=$(echo "${timestamp}/${timeslice}" | bc);
+			timestamp=$(echo "${timestamp}%${timeslice}" | bc);
 		else
-			output[${#output[*]}]=$(awk "BEGIN {printf \"%d\", $timestamp/$timeslice}");
-			timestamp=$(awk "BEGIN {printf \"%d\", $timestamp%$timeslice}");
+			output[${#output[*]}]=$(awk "BEGIN {printf \"%d\", ${timestamp}/${timeslice}}");
+			timestamp=$(awk "BEGIN {printf \"%d\", ${timestamp}%${timeslice}}");
 		fi;
 	done;
 
 	for ((i=0; i<${#output[@]}; i++));
 	do
-		if [ ${output[$i]} -gt 0 ] || [ ! -z "$time_string" ];
+		if [ ${output[${i}]} -gt 0 ] || [ ! -z "$time_string" ];
 		then
-			if [ ! -z "$time_string" ];
+			if [ ! -z "${time_string}" ];
 			then
-				time_string=$time_string" ";
+				time_string=${time_string}" ";
 			fi;
-			time_string=$time_string${output[$i]}${timenames[$i]};
+			time_string=${time_string}${output[${i}]}${timenames[${i}]};
 		fi;
 	done;
-	if [ ! -z $ms ];
+	if [ ! -z ${ms} ];
 	then
-		if [ $ms -gt 0 ];
+		if [ ${ms} -gt 0 ];
 		then
-			time_string=$time_string" "$ms"ms";
+			time_string=${time_string}" "${ms}"ms";
 		fi;
 	fi;
 	# just in case the time is 0
-	if [ -z "$time_string" ];
+	if [ -z "${time_string}" ];
 	then
 		time_string="0s";
 	fi;
-	echo -n "$time_string";
+	echo -n "${time_string}";
 }
 
 # if no verbose flag is set run, no output
 VERBOSE='--partial';
+DRY_RUN='';
 EXT_ATTRS='';
 LOG_FILE="/var/log/rsync/rsync_backup.log";
 _LOG_FILE='';
@@ -95,13 +97,16 @@ _LOG_FILE_CONTROL='';
 CHECK=1;
 
 # set options
-while getopts ":vcxs:t:l:h" opt
+while getopts ":vncxs:t:l:h" opt
 do
-    case $opt in
+    case ${opt} in
         v|verbose)
 			# verbose flag shows output
             VERBOSE='-P';
             ;;
+		n|dry-run)
+			DRY_RUN='n';
+			;;
         x|extattr)
             EXT_ATTRS='-XA';
             ;;
@@ -109,21 +114,21 @@ do
 			CHECK=0;
 			;;
         s|source)
-            if [ -z "$SOURCE" ];
+            if [ -z "${SOURCE}" ];
 			then
-				SOURCE="$OPTARG";
+				SOURCE="${OPTARG}";
 			fi;
             ;;
         t|target)
-            if [ -z "$TARGET" ];
+            if [ -z "${TARGET}" ];
 			then
-				TARGET="$OPTARG";
+				TARGET="${OPTARG}";
 			fi;
             ;;
         l|logfile)
-            if [ -z "$_LOG_FILE" ];
+            if [ -z "${_LOG_FILE}" ];
 			then
-				_LOG_FILE="$OPTARG";
+				_LOG_FILE="${OPTARG}";
 			fi;
             ;;
         h|help)
@@ -131,7 +136,7 @@ do
             exit 0;
             ;;
         \?)
-            echo -e "\n Option does not exist: $OPTARG\n";
+            echo -e "\n Option does not exist: ${OPTARG}\n";
             usage;
             exit 1;
             ;;
@@ -139,37 +144,37 @@ do
 done;
 
 # use new log file path, if the folder is ok and writeable
-if [ ! -z "$_LOG_FILE" ];
+if [ ! -z "${_LOG_FILE}" ];
 then
 	# check if this is valid writeable file
-	touch "$_LOG_FILE";
-	if [ -f "$_LOG_FILE" ];
+	touch "${_LOG_FILE}";
+	if [ -f "${_LOG_FILE}" ];
 	then
-		LOG_FILE=$_LOG_FILE;
+		LOG_FILE=${_LOG_FILE};
 		# set new control log file in the given folder
-		LOG_FILE_CONTROL=`dirname -z $LOG_FILE`"/rsync_backup.control.log";
+		LOG_FILE_CONTROL=$(dirname -z ${LOG_FILE})"/rsync_backup.control.log";
 	else
-		echo "Log file '$_LOG_FILE' is not writeable, fallback to '$LOG_FILE'";
+		echo "Log file '${_LOG_FILE}' is not writeable, fallback to '${LOG_FILE}'";
 	fi;
 fi;
 
-if [ $CHECK -eq 1 ];
+if [ ${CHECK} -eq 1 ];
 then
-	if [[ ! -d "$SOURCE" || ! -d "$TARGET" ]];
+	if [[ ! -d "${SOURCE}" || ! -d "${TARGET}" ]];
 	then
 		echo "Give source and target path.";
-		if [ ! -z "$SOURCE" ] && [ ! -d "$SOURCE" ];
+		if [ ! -z "${SOURCE}" ] && [ ! -d "${SOURCE]}" ];
 		then
-			echo "Source folder not found: $SOURCE";
+			echo "Source folder not found: ${SOURCE}";
 		fi;
-		if [ ! -z "$TARGET" ] && [ ! -d "$TARGET" ];
+		if [ ! -z "${TARGET}" ] && [ ! -d "${TARGET}" ];
 		then
-			echo "Target folder not found: $TARGET";
+			echo "Target folder not found: ${TARGET}";
 		fi;
 		exit;
 	fi;
 else
-	if [[ -z "$SOURCE" || -z "$TARGET" ]];
+	if [[ -z "${SOURCE}" || -z "${TARGET}" ]];
 	then
 		echo "Give source and target path.";
 		exit;
@@ -179,30 +184,30 @@ fi;
 LOG_CONTROL="tee -a ${LOG_FILE_CONTROL}";
 # run lock file, based on source target folder names (/ transformed to _)
 RUN_FOLDER='/var/run/';
-run_file=$RUN_FOLDER"rsync-script_"`echo "$SOURCE" | sed -e 's/[\/@\*:]/_/g'`'_'`echo "$TARGET" | sed -e 's/[\/@\*:]/_/g'`'.run';
+run_file=${RUN_FOLDER}"rsync-script_"$(echo "${SOURCE}" | sed -e 's/[\/@\*:]/_/g')'_'$(echo "${TARGET}" | sed -e 's/[\/@\*:]/_/g')'.run';
 exists=0;
-if [ -f "$run_file" ];
+if [ -f "${run_file}" ];
 then
 	# check if the pid in the run file exists, if yes, abort
-	pid=`cat "$run_file"`;
+	pid=$(cat "${run_file}");
 	while read _ps;
 	do
-		if [ $_ps -eq $pid ];
+		if [ ${_ps} -eq ${pid} ];
 		then
 			exists=1;
-			echo "Rsync script already running with pid $pid";
+			echo "Rsync script already running with pid ${pid}";
 			break;
 		fi;
 	done < <(ps xu|sed 1d|awk '{print $2}');
 	# not exited, so not running, clean up pid
-	if [ $exists -eq 0 ];
+	if [ ${exists} -eq 0 ];
 	then
-		rm -f "$run_file";
+		rm -f "${run_file}";
 	else
 		exit 0;
 	fi;
 fi;
-echo $$ > "$run_file";
+echo $$ > "${run_file}";
 
 # a: archive
 # z: compress
@@ -215,14 +220,20 @@ echo $$ > "$run_file";
 # remove -A for nfs sync, has problems with ACL data
 basic_params='-azvi --stats --delete --exclude "lost+found" -hh';
 
+if [ ! -z "${DRY_RUN}" ];
+then
+	_DRY_RUN=' [DRY RUN]';
+else
+	_DRY_RUN='';
+fi;
 script_start_time=`date +'%F %T'`;
 START=`date +'%s'`;
 PID=$$;
-echo "==> [$PID] Sync '$SOURCE' to '$TARGET', start at '$script_start_time' ..." | $LOG_CONTROL;
+echo "==> [${PID}]${_DRY_RUN} Sync '${SOURCE}' to '${TARGET}', start at '${script_start_time}' ..." | ${LOG_CONTROL};
 echo "";
-rsync $basic_params $VERBOSE $XT_ATTRS --log-file=$LOG_FILE --log-file-format="%o %i %f%L %l (%b)" $SOURCE $TARGET;
+rsync ${basic_params} ${VERBOSE} ${DRY_RUN} ${EXT_ATTRS} --log-file=${LOG_FILE} --log-file-format="%o %i %f%L %l (%b)" ${SOURCE} ${TARGET};
 echo "";
-DURATION=$[ `date +'%s'`-$START ];
-echo "<== [$PID] Finished rsync copy '$SOURCE' to '$TARGET' started at $script_start_time and finished at `date +'%F %T'` and run for `convert_time $DURATION`." | $LOG_CONTROL;
+DURATION=$[ $(date +'%s')-${START} ];
+echo "<== [${PID}]${_DRY_RUN} Finished rsync copy '${SOURCE}' to '${TARGET}' started at ${script_start_time} and finished at $(date +'%F %T') and run for $(convert_time ${DURATION})." | ${LOG_CONTROL};
 # remove lock file
-rm -f "$run_file";
+rm -f "${run_file}";
