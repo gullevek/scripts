9574cd607c9a6c59ecacea00df38f9beb565b39a
Clemens Schwaighofer <gullevek@gullevek.org>
2015-12-17 11:06:21 +0900

    Update screen init to fix TERMCAP var issue
    TERMCAP var was not completely written and some applications like mc
    showed in b/w only.
    I now export SCREENCAP var beforehand and that sort of fixes it (TERMCAP
    var is not set at all).

--- screen/screen_init.sh
+++ screen/screen_init.sh
@@ -31,14 +31,14 @@ then
 	ERROR=1;
 else
 	# get the first line for the screen name
-	SCREEN_NAME=`head -n 1 "$1"`;
+	SCREEN_NAME=$(head -n 1 "$1");
 fi;
 
 # check if we are in a screen one, if yes, exit
 if [ -z "$STY" ];
 then
 	# check if the "work" screen exists
-	if [ ! -z "$SCREEN_NAME" ] && [[ ! -z `screen -ls | grep ".$SCREEN_NAME\t"` ]];
+	if [ ! -z "$SCREEN_NAME" ] && [[ ! -z $(screen -ls | grep ".$SCREEN_NAME\t") ]];
 	then
 		echo "Screen '$SCREEN_NAME' already exists";
 		ERROR=1;
@@ -53,6 +53,32 @@ then
 	exit;
 fi;
 
+export SCREENCAP="SC|screen|VT 100/ANSI X3.64 virtual terminal:\
+:DO=\E[%dB:LE=\E[%dD:RI=\E[%dC:UP=\E[%dA:bs:bt=\E[Z:\
+:cd=\E[J:ce=\E[K:cl=\E[H\E[J:cm=\E[%i%d;%dH:ct=\E[3g:\
+:do=^J:nd=\E[C:pt:rc=\E8:rs=\Ec:sc=\E7:st=\EH:up=\EM:\
+:le=^H:bl=^G:cr=^M:it#8:ho=\E[H:nw=\EE:ta=^I:is=\E)0:\
+:li#95:co#245:am:xn:xv:LP:sr=\EM:al=\E[L:AL=\E[%dL:\
+:cs=\E[%i%d;%dr:dl=\E[M:DL=\E[%dM:dc=\E[P:DC=\E[%dP:\
+:im=\E[4h:ei=\E[4l:mi:IC=\E[%d@:ks=\E[?1h\E=:\
+:ke=\E[?1l\E>:vi=\E[?25l:ve=\E[34h\E[?25h:vs=\E[34l:\
+:ti=\E[?1049h:te=\E[?1049l:us=\E[4m:ue=\E[24m:so=\E[3m:\
+:se=\E[23m:mb=\E[5m:md=\E[1m:mh=\E[2m:mr=\E[7m:\
+:me=\E[m:ms:\
+:Co#8:pa#64:AF=\E[3%dm:AB=\E[4%dm:op=\E[39;49m:AX:\
+:vb=\Eg:G0:as=\E(0:ae=\E(B:\
+:ac=\140\140aaffggjjkkllmmnnooppqqrrssttuuvvwwxxyyzz{{||}}~~..--++,,hhII00:\
+:po=\E[5i:pf=\E[4i:Km=\E[M:k0=\E[10~:k1=\EOP:k2=\EOQ:\
+:k3=\EOR:k4=\EOS:k5=\E[15~:k6=\E[17~:k7=\E[18~:\
+:k8=\E[19~:k9=\E[20~:k;=\E[21~:F1=\E[23~:F2=\E[24~:\
+:F3=\E[1;2P:F4=\E[1;2Q:F5=\E[1;2R:F6=\E[1;2S:\
+:F7=\E[15;2~:F8=\E[17;2~:F9=\E[18;2~:FA=\E[19;2~:kb=:\
+:K2=\EOE:kB=\E[Z:kF=\E[1;2B:kR=\E[1;2A:*4=\E[3;2~:\
+:*7=\E[1;2F:#2=\E[1;2H:#3=\E[2;2~:#4=\E[1;2D:%c=\E[6;2~:\
+:%e=\E[5;2~:%i=\E[1;2C:kh=\E[1~:@1=\E[1~:kH=\E[4~:\
+:@7=\E[4~:kN=\E[6~:kP=\E[5~:kI=\E[2~:kD=\E[3~:ku=\EOA:\
+:kd=\EOB:kr=\EOC:kl=\EOD:km:";
+
 # read the config file and init the screen
 pos=0;
 cat "$1" |
@@ -64,8 +90,8 @@ do
 		SCREEN_NAME=$line;
 	else
 		# extract screen title and command (should also be cleaned for title)
-		SCREEN_TITLE=`echo $line | cut -d "#" -f 1`;
-		SCREEN_CMD=`echo $line | cut -d "#" -f 2`;
+		SCREEN_TITLE=$(echo "$line" | cut -d "#" -f 1);
+		SCREEN_CMD=$(echo "$line" | cut -d "#" -f 2);
 		# screen number is pos - 1
 		SCREEN_POS=$[ $pos-1 ];
 		# for the first screen, we need to init the screen and only set title
