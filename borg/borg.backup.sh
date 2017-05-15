30df8e2eb9d8db3283394e41ee0db564c98a18fb
Clemens Schwaighofer <clemens.schwaighofer@egplusww.com>
2017-05-15 18:59:44 +0900

    Borg script updates for prune output and un-mount
    The Borg prune part now outputs stats to STDOUT (redirect from STDERR)
    and includes the -s (stats) output too.
    
    The Borg mount script has a option for unmounting

--- borg/borg.backup.sh
+++ borg/borg.backup.sh
@@ -57,6 +57,9 @@ do
 			usage;
 			exit 0;
 			;;
+		:)
+			echo "Option -$OPTARG requires an argument."
+			;;
 		\?)
 			echo -e "\n Option does not exist: ${OPTARG}\n";
 			usage;
@@ -315,8 +318,8 @@ fi;
 echo "Prune repository with keep daily: ${KEEP_DAYS}, weekly: ${KEEP_WEEKS}, monthly: ${KEEP_MONTHS}";
 if [ ${DEBUG} -eq 1 ];
 then
-	echo "borg prune -v ${PRUNE_DEBUG} ${REPOSITORY} --keep-daily=${KEEP_DAYS} --keep-weekly=${KEEP_WEEKS} --keep-monthly=${KEEP_MONTHS}";
+	echo "borg prune -v -s ${PRUNE_DEBUG} ${REPOSITORY} --keep-daily=${KEEP_DAYS} --keep-weekly=${KEEP_WEEKS} --keep-monthly=${KEEP_MONTHS}";
 fi;
-borg prune -v ${PRUNE_DEBUG} ${REPOSITORY} --keep-daily=${KEEP_DAYS} --keep-weekly=${KEEP_WEEKS} --keep-monthly=${KEEP_MONTHS} || echo "[!] Attic prune aborted";
+borg prune -v -s ${PRUNE_DEBUG} ${REPOSITORY} --keep-daily=${KEEP_DAYS} --keep-weekly=${KEEP_WEEKS} --keep-monthly=${KEEP_MONTHS} 2>&1 || echo "[!] Attic prune aborted";
 
 ## END
--- borg/borg.mount.sh
+++ borg/borg.mount.sh
@@ -14,20 +14,23 @@ SETTINGS_FILE="borg.backup.settings";
 MOUNT_PATH="/mnt/restore/";
 # backup path to borg storage
 ATTIC_BACKUP_FILE='';
+# if we are mount or unmount (default is mount)
+UMOUNT=0;
 
 function usage ()
 {
 	cat <<- EOT
-	Usage: ${0##/*/} [-c <config folder>] [-m <mount path>] [-f <borg backup file>]
+	Usage: ${0##/*/} [-c <config folder>] [-m <mount path>] [-f <borg backup file>] [-u <mount path]
 
 	-c <config folder>: if this is not given, ${BASE_FOLDER} is used
 	-m <mount path>: where to mount the image
+	-u umount mounted image
 	-f <borg backup file>: override full path to backup file instead of using the settings info
 	EOT
 }
 
 # set options
-while getopts ":c:m:f:h" opt
+while getopts ":c:m:u:f:h" opt
 do
 	case "${opt}" in
 		c|config)
@@ -36,12 +39,18 @@ do
 		m|mount)
 			MOUNT_PATH=${OPTARG};
 			;;
+		u|umount)
+			UMOUNT=1;
+			;;
 		f|file)
 			ATTIC_BACKUP_FILE=${OPTARG};
 			;;
 		h|help)
 			usage;
 			exit 0;
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument."
 			;;
 		\?)
 			echo -e "\n Option does not exist: ${OPTARG}\n";
@@ -57,50 +66,57 @@ then
 	exit 0;
 fi;
 
-TARGET_SERVER='';
-if [ -z "${ATTIC_BACKUP_FILE}" ];
+if [ ${UMOUNT} -eq 0 ];
 then
-	if [ ! -f "${BASE_FOLDER}${SETTINGS_FILE}" ];
+	TARGET_SERVER='';
+	if [ -z "${ATTIC_BACKUP_FILE}" ];
 	then
-		echo "Cannot find ${BASE_FOLDER}${SETTINGS_FILE}";
-		exit 0;
+		if [ ! -f "${BASE_FOLDER}${SETTINGS_FILE}" ];
+		then
+			echo "Cannot find ${BASE_FOLDER}${SETTINGS_FILE}";
+			exit 0;
+		fi;
+		. ${BASE_FOLDER}${SETTINGS_FILE}
+		# set the borg backup file base on the settings data
+		# if we have user/host then we build the ssh command
+		if [ ! -z "${TARGET_USER}" ] && [ ! -z "${TARGET_HOST}" ];
+		then
+			TARGET_SERVER=${TARGET_USER}"@"${TARGET_HOST}":";
+		fi;
+		REPOSITORY=${TARGET_SERVER}${TARGET_FOLDER}${BACKUP_FILE};
+	else
+		REPOSITORY=${ATTIC_BACKUP_FILE};
 	fi;
-	. ${BASE_FOLDER}${SETTINGS_FILE}
-	# set the borg backup file base on the settings data
-	# if we have user/host then we build the ssh command
-	if [ ! -z "${TARGET_USER}" ] && [ ! -z "${TARGET_HOST}" ];
-	then
-		TARGET_SERVER=${TARGET_USER}"@"${TARGET_HOST}":";
-	fi;
-	REPOSITORY=${TARGET_SERVER}${TARGET_FOLDER}${BACKUP_FILE};
-else
-	REPOSITORY=${ATTIC_BACKUP_FILE};
-fi;
 
-# check that the repostiory exists
-REPOSITORY_OK=0;
-if [ ! -z "${TARGET_SERVER}" ];
-then
-	# remove trailing : for this
-	TARGET_SERVER=${TARGET_SERVER/:};
-	# use ssh command to check remote existense
-	if [ `ssh "${TARGET_SERVER}" "if [ -d \"${TARGET_FOLDER}${BACKUP_FILE}\" ]; then echo 1; else echo 0; fi;"` -eq 1 ];
+	# check that the repostiory exists
+	REPOSITORY_OK=0;
+	if [ ! -z "${TARGET_SERVER}" ];
+	then
+		# remove trailing : for this
+		TARGET_SERVER=${TARGET_SERVER/:};
+		# use ssh command to check remote existense
+		if [ `ssh "${TARGET_SERVER}" "if [ -d \"${TARGET_FOLDER}${BACKUP_FILE}\" ]; then echo 1; else echo 0; fi;"` -eq 1 ];
+		then
+			REPOSITORY_OK=1;
+		fi;
+	elif [ -d "${REPOSITORY}" ];
 	then
 		REPOSITORY_OK=1;
 	fi;
-elif [ -d "${REPOSITORY}" ];
-then
-	REPOSITORY_OK=1;
-fi;
 
-if [ ${REPOSITORY_OK} -eq 0 ];
-then
-	echo "Repository ${REPOSITORY} does not exists";
-	exit 0;
-fi;
+	if [ ${REPOSITORY_OK} -eq 0 ];
+	then
+		echo "Repository ${REPOSITORY} does not exists";
+		exit 0;
+	fi;
 
-echo "Mounting ${REPOSITORY} on ${MOUNT_PATH}";
-# all ok, lets mount it
-borg mount "${REPOSITORY}" "${MOUNT_PATH}";
+	echo "Mounting ${REPOSITORY} on ${MOUNT_PATH}";
+	# all ok, lets mount it
+	borg mount "${REPOSITORY}" "${MOUNT_PATH}";
+else
+	echo "Unmounting ${MOUNT_PATH}";
+	# will fail with error if not mounted, but not critical
+	borg umount "${MOUNT_PATH}";
+fi;
 
 ## END
