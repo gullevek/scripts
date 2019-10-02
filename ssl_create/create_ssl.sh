bc2f0cec2cbdf100e9938d83b27ccb464365dc3c
Clemens Schwaighofer <clemens.schwaighofer@egplusww.com>
2019-10-02 17:15:59 +0900

    ssl create info update

--- ssl_create/create_ssl.sh
+++ ssl_create/create_ssl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # creates SSL key requests from a input file
-# needs country|state|locality|organization|domain|password
+# needs country|state|locality|organization|organization unit|domain|password
 
 function usage ()
 {
