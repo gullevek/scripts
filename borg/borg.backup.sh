258cdef0f99247c549b4787bf3956afcb8047e79
Clemens Schwaighofer <clemens.schwaighofer@egplusww.com>
2021-09-09 06:54:04 +0900

    Borg Backup update with spaces in backup folders
    There where issues with spaces and special chars in backup folders
    (eg ' - ') and this was fixed.
    In this fix we also check that the TARGET folders for repository or
    remote borg do not have spaces inside (current limitation)
    Temporary file for exclude is stored in /tmp/ folder and no longer in
    the BASE_FOLDER

--- borg/borg.backup.sh
+++ borg/borg.backup.sh
@@ -24,6 +24,8 @@ VERSION="20210902-1118";
 _LOG_FOLDER="/var/log/borg.backup/";
 # log file name is set based on BACKUP_FILE, .log is added
 LOG_FOLDER="";
+# should be there on everything
+TEMPDIR="/tmp/";
 # creates borg backup based on the include/exclude files
 # if base borg folder (backup files) does not exist, it will automatically init it
 # base folder
@@ -61,6 +63,7 @@ OPT_PROGRESS="";
 OPT_LIST="";
 OPT_REMOTE="";
 OPT_LOG_FOLDER="";
+OPT_EXCLUDE="";
 # config variables (will be overwritten from .settings file)
 TARGET_USER="";
 TARGET_HOST="";
@@ -165,14 +168,12 @@ done;
 
 # add trailing slasd for base folder
 [[ "${BASE_FOLDER}" != */ ]] && BASE_FOLDER=${BASE_FOLDER}"/";
-# quote base folder
-BASE_FOLDER=$(printf "%q" ${BASE_FOLDER});
 # must have settings file there, if not, abort early
-if [ ! -f ${BASE_FOLDER}${SETTINGS_FILE} ]; then
+if [ ! -f "${BASE_FOLDER}${SETTINGS_FILE}" ]; then
 	echo "No settings file could be found: ${BASE_FOLDER}${SETTINGS_FILE}";
 	exit 1;
 fi;
-if [ ! -w ${BASE_FOLDER} ]; then
+if [ ! -w "${BASE_FOLDER}" ]; then
 	echo "Cannot write to BASE_FOLDER ${BASE_FOLDER}";
 	exit 1;
 fi;
@@ -194,7 +195,7 @@ if [ ${LIST} -eq 1 ]; then
 fi;
 
 # read config file
-. ${BASE_FOLDER}${SETTINGS_FILE};
+. "${BASE_FOLDER}${SETTINGS_FILE}";
 
 # check LOG_FOLDER, TARGET_BORG_PATH, TARGET_FOLDER must have no ~/ as start position
 if [[ ${LOG_FOLDER} =~ ^~\/ ]]; then
@@ -238,27 +239,26 @@ fi;
 # log file set and check
 # option folder overrides all other folders
 if [ ! -z "${OPT_LOG_FOLDER}" ]; then
-	LOG_FOLDER=${OPT_LOG_FOLDER};
+	LOG_FOLDER="${OPT_LOG_FOLDER}";
 fi;
 # if empty folder set to default folder
 if [ -z "${LOG_FOLDER}" ]; then
-	LOG_FOLDER=${_LOG_FOLDER};
+	LOG_FOLDER="${_LOG_FOLDER}";
 fi;
-# escape for folder exists test and folder creation
-__LOG_FOLDER=$(printf "%q" ${LOG_FOLDER});
-# if this fails, it will throw an error message
-if [ ! -d $__LOG_FOLDER ]; then
-	mkdir ${__LOG_FOLDER};
+# if folder does not exists create it
+if [ ! -d "${LOG_FOLDER}" ]; then
+	mkdir "${LOG_FOLDER}";
 fi;
 # set the output log folder
-LOG=$(printf "%q" ${LOG_FOLDER}"/"${BACKUP_FILE}".log");
+# LOG=$(printf "%q" "${LOG_FOLDER}/${BACKUP_FILE}.log");
+LOG="${LOG_FOLDER}/${BACKUP_FILE}.log";
 # fail if not writeable to folder or file
-if [[ -f ${LOG} && ! -w ${LOG} ]] || [[ ! -f ${LOG} && ! -w ${__LOG_FOLDER} ]]; then
+if [[ -f "${LOG}" && ! -w "${LOG}" ]] || [[ ! -f "${LOG}" && ! -w "${LOG_FOLDER}" ]]; then
 	echo "Log folder or log file is not writeable: ${LOG}";
 	exit 1;
 fi;
 # start logging from here
-exec &> >(tee -a ${LOG});
+exec &> >(tee -a "${LOG}");
 echo "=== [START : $(date +'%F %T')] ===========================================>";
 # show info for version always
 echo "Script version: ${VERSION}";
@@ -283,13 +283,28 @@ fi;
 
 # remote borg path
 if [ ! -z "${TARGET_BORG_PATH}" ]; then
-	OPT_REMOTE="--remote-path "$(printf "%q" "${TARGET_BORG_PATH}");
+	if [[ "${TARGET_BORG_PATH}" =~ \ |\' ]]; then
+		echo "Space found in ${TARGET_BORG_PATH}. Aborting";
+		echo "There are issues with passing on paths with spaces"
+		echo "as parameters"
+		exit;
+	fi;
+	OPT_REMOTE="--remote-path="$(printf "%q" "${TARGET_BORG_PATH}");
 fi;
 
 if [ -z "${TARGET_FOLDER}" ]; then
 	echo "[! $(date +'%F %T')] No target folder has been set yet";
 	exit 1;
 else
+	# There are big issues with TARGET FOLDERS with spaces
+	# we should abort anything with this
+	if [[ "${TARGET_FOLDER}" =~ \ |\' ]]; then
+		echo "Space found in ${TARGET_FOLDER}. Aborting";
+		echo "There is some problem with passing paths with spaces as";
+		echo "repository base folder"
+		exit;
+	fi;
+
 	# This does not care for multiple trailing or leading slashes
 	# it just makes sure we have at least one set
 	# for if we have a single slash, remove it
@@ -305,20 +320,21 @@ TARGET_SERVER='';
 # user@host OR ssh://user@host:port/ IF TARGET_PORT is set
 # user/host/port
 if [ ! -z "${TARGET_USER}" ] && [ ! -z "${TARGET_HOST}" ] && [ ! -z "${TARGET_PORT}" ]; then
-	TARGET_SERVER="ssh://"${TARGET_USER}"@"${TARGET_HOST}":"${TARGET_PORT}"/";
+	TARGET_SERVER="ssh://${TARGET_USER}@${TARGET_HOST}:${TARGET_PORT}/";
 # host/port
 elif [ ! -z "${TARGET_HOST}" ] && [ ! -z "${TARGET_PORT}" ]; then
-	TARGET_SERVER="ssh://"${TARGET_HOST}":"${TARGET_PORT}"/";
+	TARGET_SERVER="ssh://${TARGET_HOST}:${TARGET_PORT}/";
 # user/host
 elif [ ! -z "${TARGET_USER}" ] && [ ! -z "${TARGET_HOST}" ]; then
-	TARGET_SERVER=${TARGET_USER}"@"${TARGET_HOST}":";
+	TARGET_SERVER="${TARGET_USER}@${TARGET_HOST}:";
 # host
 elif [ ! -z "${TARGET_HOST}" ]; then
-	TARGET_SERVER=${TARGET_HOST}":";
+	TARGET_SERVER="${TARGET_HOST}:";
 fi;
-REPOSITORY=${TARGET_SERVER}${TARGET_FOLDER}${BACKUP_FILE};
+# we dont allow special characters, so we don't need to special escape it
+REPOSITORY="${TARGET_SERVER}${TARGET_FOLDER}${BACKUP_FILE}";
 
-if [ ! -f ${BASE_FOLDER}${INCLUDE_FILE} ]; then
+if [ ! -f "${BASE_FOLDER}${INCLUDE_FILE}" ]; then
 	echo "[! $(date +'%F %T')] The include folder file ${INCLUDE_FILE} is missing";
 	exit 1;
 fi;
@@ -327,7 +343,7 @@ fi;
 if [ ! -z "${COMPRESSION}" ]; then
 	# valid compress
 	if [ "${COMPRESSION}" = "lz4" ] || [ "${COMPRESSION}" = "zlib" ] || [ "${COMPRESSION}" = "lzma" ]; then
-		OPT_COMPRESSION="-C ${COMPRESSION}";
+		OPT_COMPRESSION="-C=${COMPRESSION}";
 		# if COMPRESSION_LEVEL, check it is a valid regex
 		# ignore it if this is lz4
 		if [ ! -z "${COMPRESSION_LEVEL}" ] && [ "${COMPRESSION}" != "lz4" ]; then
@@ -351,38 +367,38 @@ fi;
 
 # build options and info string,
 # also flag BACKUP_SET check if hourly is set
-KEEP_OPTIONS="";
+KEEP_OPTIONS=();
 KEEP_INFO="";
 BACKUP_SET_CHECK=0;
 if [ ${KEEP_LAST} -gt 0 ]; then
-	KEEP_OPTIONS="${KEEP_OPTIONS} --keep-last ${KEEP_LAST}";
+	KEEP_OPTIONS+=("--keep-last=${KEEP_LAST}");
 	KEEP_INFO="${KEEP_INFO}, last: ${KEEP_LAST}";
 fi;
 if [ ${KEEP_HOURS} -gt 0 ]; then
-	KEEP_OPTIONS="${KEEP_OPTIONS} --keep-hourly ${KEEP_HOURS}";
+	KEEP_OPTIONS+=("--keep-hourly=${KEEP_HOURS}");
 	KEEP_INFO="${KEEP_INFO}, hourly: ${KEEP_HOURS}";
 	BACKUP_SET_CHECK=1;
 fi;
 if [ ${KEEP_DAYS} -gt 0 ]; then
-	KEEP_OPTIONS="${KEEP_OPTIONS} --keep-daily ${KEEP_DAYS}";
+	KEEP_OPTIONS+=("--keep-daily=${KEEP_DAYS}");
 	KEEP_INFO="${KEEP_INFO}, daily: ${KEEP_DAYS}";
 fi;
 if [ ${KEEP_WEEKS} -gt 0 ]; then
-	KEEP_OPTIONS="${KEEP_OPTIONS} --keep-weekly ${KEEP_WEEKS}";
+	KEEP_OPTIONS+=("--keep-weekly=${KEEP_WEEKS}");
 	KEEP_INFO="${KEEP_INFO}, weekly: ${KEEP_WEEKS}";
 fi;
 if [ ${KEEP_MONTHS} -gt 0 ]; then
-	KEEP_OPTIONS="${KEEP_OPTIONS} --keep-monthly ${KEEP_MONTHS}";
+	KEEP_OPTIONS+=("--keep-monthly=${KEEP_MONTHS}");
 	KEEP_INFO="${KEEP_INFO}, monthly: ${KEEP_MONTHS}";
 fi;
 if [ ${KEEP_YEARS} -gt 0 ]; then
-	KEEP_OPTIONS="${KEEP_OPTIONS} --keep-yearly ${KEEP_YEARS}";
+	KEEP_OPTIONS+=("--keep-yearly=${KEEP_YEARS}");
 	KEEP_INFO="${KEEP_INFO}, yearly: ${KEEP_YEARS}";
 fi;
 if [ ! -z "${KEEP_WITHIN}" ]; then
 	# check for invalid string. can only be number + H|d|w|m|y
 	if [[ "${KEEP_WITHIN}" =~ ^[0-9]+[Hdwmy]{1}$ ]]; then
-		KEEP_OPTIONS="${KEEP_OPTIONS} --keep-within ${KEEP_WITHIN}";
+		KEEP_OPTIONS+=("--keep-within=${KEEP_WITHIN}");
 		KEEP_INFO="${KEEP_INFO}, within: ${KEEP_WITHIN}";
 		if [[ "${KEEP_WITHIN}" == *"H"* ]]; then
 			BACKUP_SET_CHECK=1;
@@ -397,8 +413,6 @@ if [ -z "${KEEP_OPTIONS}" ]; then
 	echo "[! $(date +'%F %T')] It seems no KEEP_* entries where set in a valid format.";
 	exit 1;
 fi;
-# remove the first , (first character)
-KEEP_INFO=$(echo "${KEEP_INFO}" | cut -c 2-);
 # set BACKUP_SET if empty, check for for DATE is set
 if [ -z "${BACKUP_SET}" ]; then
 	# DATE is deprecated and will be removed
@@ -416,9 +430,12 @@ if [ ${BACKUP_SET_CHECK} -eq 1 ] && [[ "${BACKUP_SET}" != *"%H"* ]]; then
 	BACKUP_SET=$(echo "${BACKUP_SET}" | sed -e "s/}/T%H:%M:%S}/");
 fi;
 
+# for folders list split set to "#" and keep the old setting as is
+_IFS=${IFS};
+IFS="#";
 # general borg settings
 # set base path to config directory to keep cache/config separated
-export BORG_BASE_DIR=${BASE_FOLDER};
+export BORG_BASE_DIR="${BASE_FOLDER}";
 # ignore non encrypted access
 export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=${_BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK};
 # ignore moved repo access
@@ -427,10 +444,12 @@ export BORG_RELOCATED_REPO_ACCESS_IS_OK=${_BORG_RELOCATED_REPO_ACCESS_IS_OK};
 if [ ${DEBUG} -eq 1 ]; then
 	echo "export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=${_BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK};";
 	echo "export BORG_RELOCATED_REPO_ACCESS_IS_OK=${_BORG_RELOCATED_REPO_ACCESS_IS_OK};";
-	echo "export BORG_BASE_DIR=${BASE_FOLDER};";
+	echo "export BORG_BASE_DIR=\"${BASE_FOLDER}\";";
 fi;
-
-# if the repository is not there, call init to create it
+# prepare debug commands only
+COMMAND_EXPORT="export BORG_BASE_DIR=\"${BASE_FOLDER}\";"
+COMMAND_INFO="${COMMAND_EXPORT}borg info ${OPT_REMOTE} ${REPOSITORY}";
+# if the is not there, call init to create it
 # if this is user@host, we need to use ssh command to check if the file is there
 # else a normal check is ok
 # unless explicit given, check is skipped
@@ -453,21 +472,20 @@ if [ ${CHECK} -eq 1 ] || [ ${INIT} -eq 1 ]; then
 	fi;
 	# if check but no init and repo is there but init file is missing set it
 	if [ ${CHECK} -eq 1 ] && [ ${INIT} -eq 0 ] && [ ${INIT_REPOSITORY} -eq 0 ] &&
-		[ ! -f ${BASE_FOLDER}${BACKUP_INIT_CHECK} ]; then
+		[ ! -f "${BASE_FOLDER}${BACKUP_INIT_CHECK}" ]; then
 		# write init file
 		echo "[!] Add missing init check file";
-		echo "$(date +%s)" > ${BASE_FOLDER}${BACKUP_INIT_CHECK};
+		echo "$(date +%s)" > "${BASE_FOLDER}${BACKUP_INIT_CHECK}";
 	fi;
 	# end if checked but repository is not here
 	if [ ${CHECK} -eq 1 ] && [ ${INIT} -eq 0 ] && [ ${INIT_REPOSITORY} -eq 1 ]; then
 		echo "[! $(date +'%F %T')] No repository. Please run with -I flag to initialze repository";
-		export -n BORG_BASE_DIR BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK BORG_RELOCATED_REPO_ACCESS_IS_OK;
 		exit 1;
 	fi;
 	if [ ${EXIT} -eq 1 ] && [ ${CHECK} -eq 1 ] && [ ${INIT} -eq 0 ]; then
 		echo "Repository exists";
 		echo "For more information run:"
-		echo "export BORG_BASE_DIR=${BASE_FOLDER};borg info ${OPT_REMOTE} ${REPOSITORY}";
+		echo "${COMMAND_INFO}";
 		echo "=== [END  : $(date +'%F %T')] ============================================>";
 		exit;
 	fi;
@@ -480,10 +498,10 @@ if [ ${INIT} -eq 1 ] && [ ${INIT_REPOSITORY} -eq 1 ]; then
 		# should trap and exit properly here
 		borg init ${OPT_REMOTE} -e ${ENCRYPTION} ${OPT_VERBOSE} ${REPOSITORY};
 		# write init file
-		echo "$(date +%s)" > ${BASE_FOLDER}${BACKUP_INIT_CHECK};
+		echo "$(date +%s)" > "${BASE_FOLDER}${BACKUP_INIT_CHECK}";
 		echo "Repository initialized";
 		echo "For more information run:"
-		echo "export BORG_BASE_DIR=${BASE_FOLDER};borg info ${OPT_REMOTE} ${REPOSITORY}";
+		echo "${COMMAND_INFO}";
 	fi
 	echo "=== [END  : $(date +'%F %T')] ============================================>";
 	# exit after init
@@ -491,19 +509,21 @@ if [ ${INIT} -eq 1 ] && [ ${INIT_REPOSITORY} -eq 1 ]; then
 elif [ ${INIT} -eq 1 ] && [ ${INIT_REPOSITORY} -eq 0 ]; then
 	echo "[! $(date +'%F %T')] Repository already initialized";
 	echo "For more information run:"
-	echo "export BORG_BASE_DIR=${BASE_FOLDER};borg info ${OPT_REMOTE} ${REPOSITORY}";
+	echo "${COMMAND_INFO}";
 	exit 1;
 fi;
 
 # check for init file
-if [ ! -f ${BASE_FOLDER}${BACKUP_INIT_CHECK} ]; then
+if [ ! -f "${BASE_FOLDER}${BACKUP_INIT_CHECK}" ]; then
 	echo "[! $(date +'%F %T')] It seems the repository has never been initialized."
 	echo "Please run -I to initialize or if already initialzed run with -C for init update."
 	exit 1;
 fi;
 
-# base command
-COMMAND="borg create ${OPT_REMOTE} -v ${OPT_LIST} ${OPT_PROGRESS} ${OPT_COMPRESSION} -s ${REPOSITORY}::${BACKUP_SET}";
+# folders to backup
+FOLDERS=();
+# this if for debug output with quoted folders
+FOLDERS_Q=();
 # include list
 while read include_folder; do
 	# strip any leading spaces from that folder
@@ -527,13 +547,16 @@ while read include_folder; do
 					FOLDER_OK=1;
 					echo "+ [I] Backup folder with folder path glob '${include_folder}'";
 					# glob (*) would be escape so we replace it with a temp part and then reinsert it
-					COMMAND=${COMMAND}" "$(printf "%q" $(echo "${include_folder}" | sed 's/\*/_STARGLOB_/g') | sed 's/_STARGLOB_/\*/g');
+					FOLDERS_Q+=($(printf "%q" "$(echo "${include_folder}" | sed 's/\*/_STARGLOB_/g')" | sed 's/_STARGLOB_/\*/g'));
+					FOLDERS+=("${include_folder}");
 				elif [ ! -d "${_include_folder}" ]; then
 					echo "- [I] Backup folder with glob '${include_folder}' does not exist or is not accessable";
 				else
 					FOLDER_OK=1;
 					echo "+ [I] Backup folder with glob '${include_folder}'";
-					COMMAND=${COMMAND}" "$(printf "%q" ${include_folder});
+					# we need glob fix
+					FOLDERS_Q+=($(printf "%q" "$(echo "${include_folder}" | sed 's/\*/_STARGLOB_/g')" | sed 's/_STARGLOB_/\*/g'));
+					FOLDERS+=("${include_folder}");
 				fi;
 			# normal folder
 			elif [ ! -d "${include_folder}" ] && [ ! -e "${include_folder}" ]; then
@@ -552,16 +575,19 @@ while read include_folder; do
 				else
 					echo "+ [I] Backup folder or file '${include_folder}'";
 				fi;
-				COMMAND=${COMMAND}" "$(printf "%q" ${include_folder});
+				FOLDERS_Q+=($(printf "%q" "${include_folder}"));
+				FOLDERS+=("${include_folder}");
 			fi;
 		fi;
 	fi;
-done<${BASE_FOLDER}${INCLUDE_FILE};
+done<"${BASE_FOLDER}${INCLUDE_FILE}";
+
 # exclude list
-if [ -f ${BASE_FOLDER}${EXCLUDE_FILE} ]; then
-	# check that the folders in that exclude file are actually valid, remove non valid ones and warn
-	#TMP_EXCLUDE_FILE=$(mktemp --tmpdir ${EXCLUDE_FILE}.XXXXXXXX); #non mac
-	TMP_EXCLUDE_FILE=$(mktemp ${BASE_FOLDER}${EXCLUDE_FILE}.XXXXXXXX);
+if [ -f "${BASE_FOLDER}${EXCLUDE_FILE}" ]; then
+	# check that the folders in that exclude file are actually valid,
+	# remove non valid ones and warn
+	#TMP_EXCLUDE_FILE=$(mktemp --tmpdir ${EXCLUDE_FILE}.XXXXXXXX); # non mac
+	TMP_EXCLUDE_FILE=$(mktemp "${TEMPDIR}${EXCLUDE_FILE}".XXXXXXXX);
 	while read exclude_folder; do
 		# strip any leading spaces from that folder
 		exclude_folder=$(echo "${exclude_folder}" | sed -e 's/^[ \t]*//');
@@ -608,16 +634,23 @@ if [ -f ${BASE_FOLDER}${EXCLUDE_FILE} ]; then
 				fi;
 			fi;
 		fi;
-	done<${BASE_FOLDER}${EXCLUDE_FILE};
-	COMMAND=${COMMAND}" --exclude-from ${TMP_EXCLUDE_FILE}";
+	done<"${BASE_FOLDER}${EXCLUDE_FILE}";
+	# avoid blank file add by checking if the tmp file has a size >0
+	if [ -s "${BASE_FOLDER}${EXCLUDE_FILE}" ]; then
+		OPT_EXCLUDE="--exclude-from=${TMP_EXCLUDE_FILE}";
+	fi;
 fi;
-
+# add the repository set before we add the folders
+# base command
+COMMAND="borg create -v ${OPT_LIST} ${OPT_PROGRESS} ${OPT_COMPRESSION} -s ${OPT_REMOTE} ${OPT_EXCLUDE} ";
+# add repoistory, after that the folders will be added on call
+COMMAND=${COMMAND}${REPOSITORY}::${BACKUP_SET};
 # if info print info and then abort run
 if [ ${INFO} -eq 1 ]; then
 	echo "--- [INFO  : $(date +'%F %T')] ------------------------------------------->";
 	# show command on debug or dry run
 	if [ ${DEBUG} -eq 1 ] || [ ${DRYRUN} -eq 1 ]; then
-		echo "export BORG_BASE_DIR=${BASE_FOLDER};borg info ${OPT_REMOTE} ${REPOSITORY}";
+		echo "export BORG_BASE_DIR=\"${BASE_FOLDER}\";borg info ${OPT_REMOTE} ${REPOSITORY}";
 	fi;
 	# run info command if not a dry drun
 	if [ ${DRYRUN} -eq 0 ]; then
@@ -625,7 +658,8 @@ if [ ${INFO} -eq 1 ]; then
 	fi;
 	if [ $FOLDER_OK -eq 1 ]; then
 		echo "--- [Run command]:";
-		echo "export BORG_BASE_DIR=${BASE_FOLDER};${COMMAND}";
+		#IFS="#";
+		echo "export BORG_BASE_DIR=\"${BASE_FOLDER}\";${COMMAND} "${FOLDERS_Q[*]};
 	else
 		echo "[!] No folders where set for the backup";
 	fi;
@@ -641,14 +675,15 @@ if [ $FOLDER_OK -eq 1 ]; then
 	echo "--- [BACKUP: $(date +'%F %T')] ------------------------------------------->";
 	# show command
 	if [ ${DEBUG} -eq 1 ]; then
-		echo ${COMMAND};
+		echo $(echo ${COMMAND} | sed -e 's/[ ][ ]*/ /g') ${FOLDERS_Q[*]};
 	fi;
 	# execute backup command
 	if [ ${DRYRUN} -eq 1 ]; then
 		PRUNE_DEBUG="--dry-run";
 	else
 		# need to redirect std error to std out so all data is printed to the correct pipe
-		${COMMAND} 2>&1 || echo "[!] Attic backup aborted.";
+		# for the IFS="#" to work we need to replace options spaces with exactly ONE #
+		$(echo "${COMMAND}" | sed -e 's/[ ][ ]*/#/g') ${FOLDERS[*]} 2>&1 || echo "[!] Attic backup aborted.";
 	fi;
 	# remove the temporary exclude file if it exists
 	if [ -f "${TMP_EXCLUDE_FILE}" ]; then
@@ -661,11 +696,14 @@ fi;
 
 # clean up, always verbose
 echo "--- [PRUNE : $(date +'%F %T')] ------------------------------------------->";
-echo "Prune repository with keep${KEEP_INFO}";
+# build command
+COMMAND="borg prune ${OPT_REMOTE} -v -s --list ${PRUNE_DEBUG} ${KEEP_OPTIONS[*]} ${REPOSITORY}";
+echo "Prune repository with keep${KEEP_INFO:1}";
 if [ ${DEBUG} -eq 1 ]; then
-	echo "borg prune ${OPT_REMOTE} -v -s --list ${PRUNE_DEBUG} ${REPOSITORY} ${KEEP_OPTIONS}";
+	echo "${COMMAND//#/ }" | sed -e 's/[ ][ ]*/ /g';
 fi;
-borg prune ${OPT_REMOTE} -v -s --list ${PRUNE_DEBUG} ${REPOSITORY} ${KEEP_OPTIONS} 2>&1 || echo "[!] Attic prune aborted";
+# for the IFS="#" to work we need to replace options spaces with exactly ONE #
+$(echo "${COMMAND}" | sed -e 's/[ ][ ]*/#/g') 2>&1 || echo "[!] Attic prune aborted";
 
 echo "=== [END  : $(date +'%F %T')] ============================================>";
 
